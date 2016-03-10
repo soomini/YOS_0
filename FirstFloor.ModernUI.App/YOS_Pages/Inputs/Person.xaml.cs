6c1343934f0822404dbc82125451ec26468cfd85
성혜 이 <tudtud94@hanmail.net>
2016-03-10 17:20:12 +0900

    DB - add - TextBox를 통한 Insert기능 추가 - 왼쪽 StackPanel의 control들에 강사정보를 입력하고 등록버튼 클릭이벤트로 강사데이터 INSERT 기능을 추가함

--- FirstFloor.ModernUI.App/YOS_Pages/Inputs/Person.xaml.cs
+++ FirstFloor.ModernUI.App/YOS_Pages/Inputs/Person.xaml.cs
@@ -38,15 +38,14 @@ namespace FirstFloor.ModernUI.App.YOS_Pages.Inputs
         private string connStr = "User Id=scott;Password=tiger;Data Source=ORCL";
         #endregion
 
+        private string strGENDER="";
+
         public Person()
         {
             InitializeComponent();
             
-            this.Loaded += OnLoaded;           
-        }
+            this.Loaded += OnLoaded;
 
-        void OnLoaded(object sender, RoutedEventArgs e)
-        {
             #region 데이터 가져오기 및 DataGrid에 추가
             oraDA = new OracleDataAdapter("SELECT * FROM LECTURER", connStr);
 
@@ -56,9 +55,51 @@ namespace FirstFloor.ModernUI.App.YOS_Pages.Inputs
 
             DG1.ItemsSource = Lecturer_DS.Tables["LECTURER"].DefaultView;
             #endregion
+        }
 
+        void OnLoaded(object sender, RoutedEventArgs e)
+        {
             // select first control on the form
             Keyboard.Focus(this.TextFirstName);            
         }
+
+        private void btn_INSERT_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                #region set nuwRow value
+                DataRow newInsertRow = Lecturer_DS.Tables["LECTURER"].NewRow();
+                // 시퀀스 사용으로 수정 필요
+                newInsertRow["LECTURERNO"] = 4;
+                newInsertRow["LECTURERNAME"] = TextFirstName.Text.Trim();
+                newInsertRow["JOB"] = TextLastName.Text.Trim();
+                newInsertRow["GENDER"] = strGENDER;                
+                newInsertRow["PHONENUMBER"] = TextCity.Text.Trim();
+                newInsertRow["BIRTHDATE"] = DateBirth.SelectedDate.Value.ToString();              
+                newInsertRow["CITY"] = ((ComboBoxItem)ComboState.SelectedItem).Content.ToString();
+                newInsertRow["DETAILADDRESS"] = TextAddress.Text.Trim();
+
+                Lecturer_DS.Tables["LECTURER"].Rows.Add(newInsertRow);
+                #endregion
+                oraDA.Update(Lecturer_DS, "LECTURER");
+                MessageBox.Show("추가 성공");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("오류 : " + ex.ToString());
+            }
+        }
+
+        #region Radio value get
+        private void RadioGendeWan_Checked(object sender, RoutedEventArgs e)
+        {
+            strGENDER = (string)(sender as RadioButton).Content;
+        }
+
+        private void RadioGendeWoman_Checked(object sender, RoutedEventArgs e)
+        {
+            strGENDER = (string)(sender as RadioButton).Content;
+        }
+        #endregion        
     }
 }
