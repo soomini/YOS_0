179a48b5c36ab0a4d59deae88c08f257aa3c72b5
ysh <design.ysh@gmail.com>
2016-03-13 18:53:13 +0900

    WPF - add - Course 항목 추가 - 추가 중 중간 저장

---
+++ FirstFloor.ModernUI.App/YOS_Pages/Course_Pages/Complete.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstFloor.ModernUI.App.YOS_Pages.Course_Pages
+{
+    /// <summary>
+    /// Interaction logic for LayoutTab.xaml
+    /// </summary>
+    public partial class Complete: UserControl
+    {
+        public Complete()
+        {
+            InitializeComponent();
+
+			
+        }
+    }
+}
