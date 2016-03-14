fe52ccddb8e18d42efffc87956d379ed20c19e49
ysh <design.ysh@gmail.com>
2016-03-14 10:51:24 +0900

    WPF - add - 중간 수정 결과 저장 - 수정 중 백업 용도 저장

--- FirstFloor.ModernUI.App/YOS_Pages/Course_Pages/Ongoing.xaml.cs
+++ FirstFloor.ModernUI.App/YOS_Pages/Course_Pages/Ongoing.xaml.cs
@@ -1,47 +1,28 @@
 ﻿using FirstFloor.ModernUI.App.Content;
 using FirstFloor.ModernUI.Windows.Controls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FirstFloor.ModernUI.App.YOS_Pages.Course_Pages
 {
-    /// <summary>
-    /// Interaction logic for LayoutTab.xaml
-    /// </summary>
-    public partial class Ongoing: UserControl
+
+	public partial class Ongoing: UserControl
     {
         public Ongoing()
         {
             InitializeComponent();
         }
 
-
 		private void CommonDialog_Click(object sender, RoutedEventArgs e)
 		{
 			var dlg = new ModernDialog
 			{
 				Title = "Common dialog",
-				//Content = new LoremIpsum()
+				Content = new YOS_Content.AddCost()
 			};
-			//dlg.Content = new LoremIpsum();
-			//dlg.Resources = 
 			dlg.Buttons = new Button[] { dlg.OkButton, dlg.CancelButton };
+			
 			dlg.ShowDialog();
-
-			//this.dialogResult.Text = dlg.DialogResult.HasValue ? dlg.DialogResult.ToString() : "<null>";
-			//this.dialogMessageBoxResult.Text = dlg.MessageBoxResult.ToString();
 		}
 	}
 }
