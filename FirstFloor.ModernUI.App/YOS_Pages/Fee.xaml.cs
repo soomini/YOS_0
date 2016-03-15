318ad9554b219d2f2a31251cbb05f4f6f7006c3e
ysh <design.ysh@gmail.com>
2016-03-15 12:37:14 +0900

    WPF - add - 현황/수수료/파트너별 창 추가 - 창 추가

---
+++ FirstFloor.ModernUI.App/YOS_Pages/Fee.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace FirstFloor.ModernUI.App.YOS_Pages
+{
+	public partial class Fee : UserControl
+    {
+        public Fee()
+        {
+            InitializeComponent();
+        }
+    }
+}
