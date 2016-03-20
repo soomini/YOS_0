e85c01a7ad44be84f209e15b9d382c1e798ae364
ysh <design.ysh@gmail.com>
2016-03-20 14:21:51 +0900

    WPF - fix - Course : 역할 필터 페이지 수정 - 역할 필터 페이지 수정 완료 / 사용자정의 필터 페이지 삭제

---
+++ FirstFloor.ModernUI.App/YOS_Pages/Course_Pages/Courses/Role.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace FirstFloor.ModernUI.App.YOS_Pages.Course_Pages.Courses
+{
+
+	public partial class Role: UserControl
+    {
+        public Role()
+        {
+            InitializeComponent();
+        }
+    }
+}
