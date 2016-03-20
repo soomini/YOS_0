337c3a60e0660711bd2fd225c22369a7d595ddaa
ysh <design.ysh@gmail.com>
2016-03-20 18:14:06 +0900

    WPF - fix - 네임스페이스 및 파일명 정리 - 전체적으로 이름이 변경되었음

---
+++ FirstFloor.ModernUI.App/YOS_Pages/Status/CostStatus.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace FirstFloor.ModernUI.App.YOS_Pages.Status_Pages
+{
+	public partial class CostStatus : UserControl
+    {
+        public CostStatus()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FirstFloor.ModernUI.App/YOS_Pages/Status/CostStatus_Pages/Patners.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace FirstFloor.ModernUI.App.YOS_Pages.Status.CostStatus_Pages
+{
+
+	public partial class Patners: UserControl
+    {
+        public Patners()
+        {
+            InitializeComponent();
+        }
+    }
+}
