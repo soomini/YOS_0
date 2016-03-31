bee0c40de15ba248e259bcf9988b641ef2ec6df0
soomini <ksm385@gmail.com>
2016-03-31 21:06:16 +0900

    커밋

--- FirstFloor.ModernUI.App/YOS_Pages/Course/Complete_Pages/Role.xaml.cs
+++ FirstFloor.ModernUI.App/YOS_Pages/Course/Complete_Pages/Role.xaml.cs
@@ -26,7 +26,7 @@ namespace FirstFloor.ModernUI.App.YOS_Pages.Course.Complete_Pages
 
             Adpt = new OracleDataAdapter("SELECT * FROM LECTURE", strOraConn);
 
-            DataTable PERSON_dt = LECTURE_DS.Tables["LECTURE_dt"];
+            DataTable LECTURE_dt = LECTURE_DS.Tables["LECTURE_dt"];
 
             oraBuilder = new OracleCommandBuilder(Adpt);
 
@@ -97,6 +97,35 @@ namespace FirstFloor.ModernUI.App.YOS_Pages.Course.Complete_Pages
             DG_CO_R.ItemsSource = LECTURE_DS.Tables["LECTURE_dt"].DefaultView;
             //CBOXLEC.ItemsSource = LECTURE_DS.Tables["LECTURE_dt"].DefaultView;
             DG_CO_R.CanUserAddRows = false;
+        }
+
+        private void LB_PM_Selected(object sender, RoutedEventArgs e)
+        {
+            Adpt = new OracleDataAdapter("SELECT p.NAME, l.CLOSEDATE, l.LECTURENAME, l.LECTUREFEE FROM LECTURE l, PARTNERS p WHERE l.PROJMANAGER=p.NAME", strOraConn);
+
+            DataTable LECTURE_dt = LECTURE_DS.Tables["LECTURE_dt"];
+
+            DG_CO_R.ItemsSource = null;
+            //CBOXLEC.ItemsSource = null;
+
+            LECTURE_dt.Clear();
+
+            oraBuilder = new OracleCommandBuilder(Adpt);
+
+            Adpt.Fill(LECTURE_DS, "LECTURE_dt");
+            DG_CO_R.ItemsSource = LECTURE_DS.Tables["LECTURE_dt"].DefaultView;
+            //CB_ST_C.ItemsSource = LECTURE_DS.Tables["LECTURE_dt"].DefaultView;
+            DG_CO_R.CanUserAddRows = false;
+        }
+
+        private void LB_INT_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LB_LEC_Selected(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
