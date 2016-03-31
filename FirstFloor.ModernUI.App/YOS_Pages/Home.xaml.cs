724f1e76940cc5d335919f0418345e39a8dee2f9
ysh <design.ysh@gmail.com>
2016-03-31 20:33:52 +0900

    WPF - add - 강좌정보 중 강사 기입란 추가

--- FirstFloor.ModernUI.App/YOS_Pages/Home.xaml.cs
+++ FirstFloor.ModernUI.App/YOS_Pages/Home.xaml.cs
@@ -24,5 +24,47 @@ namespace FirstFloor.ModernUI.App.YOS_Pages
         {
             InitializeComponent();
         }
-    }
+
+		private MenuItem CreateCourseMenu(string header)
+		{
+			var item = new MenuItem { Header = header };
+			item.Items.Add("진행 중 강좌");
+			item.Items.Add(new Separator());
+			item.Items.Add("전체 진행 완료 강좌");
+			item.Items.Add("파트너별 진행 완료 강좌");
+			item.Items.Add("역할별 진행 완료 강좌");
+			return item;
+		}
+
+		private MenuItem CreateStatusMenu(string header)
+		{
+			var item = new MenuItem { Header = header };
+			item.Items.Add("강좌별 수수료 현황");
+			item.Items.Add("파트너별 수수료 현황");
+			return item;
+		}
+
+		private MenuItem CreateReferenceMenu(string header)
+		{
+			var item = new MenuItem { Header = header };
+			item.Items.Add("Patners");
+			item.Items.Add("항목별 요율");
+			item.Items.Add("강좌 분류");
+			item.Items.Add("교구 단가");
+			return item;
+		}
+
+
+		private void btn1_Click(object sender, RoutedEventArgs e)
+		{
+
+			var contextMenu = new ContextMenu();
+
+			contextMenu.Items.Add(CreateCourseMenu("Course"));
+			contextMenu.Items.Add(CreateStatusMenu("Status"));
+			contextMenu.Items.Add(CreateReferenceMenu("Reference"));
+
+			contextMenu.IsOpen = true;
+		}
+	}
 }
